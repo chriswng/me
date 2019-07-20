6fbfed66949e8742e5774571637dbb3583aee615
Christopher Yang Wang <yangwang@Christophers-MacBook-Air-2.local>
2019-07-20 21:26:13 +1000

    testing wordy pyramid logic w/o api :anguished:

--- week4/c.py
+++ week4/c.py
@@ -3,14 +3,14 @@ templist2 = ['3', '5', '7' ,'9','11','13','15','17','19']
 wordlist=[]
 
 
-# for i in range(len(templist)):
-#     wordlist.append(templist[i])
-# templist2.reverse()
-# for i in range(1,len(templist2)):
-#     wordlist.append(templist2[i])
-# print(wordlist)
-for i in range(3,20,2):
-    print(i)
-wordlist.append(templist)
-wordlist.append(templist2)
+for i in range(len(templist)):
+    wordlist.append(templist[i])
+templist2.reverse()
+for i in range(1,len(templist2)):
+    wordlist.append(templist2[i])
 print(wordlist)
+# for i in range(3,20,2):
+#     print(i)
+# wordlist.append(templist)
+# wordlist.append(templist2)
+# print(wordlist)
